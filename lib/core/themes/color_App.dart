4f5fda4997efa0565b56fcec6677ff1916e1b7b6
marknagy14 <1900520@eng.asu.edu.eg>
2024-11-14 18:25:15 +0200

    created system design (styles, colors and assets)

---
+++ lib/core/themes/color_App.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+
+abstract class ColorApp {
+  static Color primaryColor = const Color(0x25D366);
+  static Color secondaryColor = const Color(0x128C7E);
+  static Color textColor = const Color(0x00000);
+
+}
